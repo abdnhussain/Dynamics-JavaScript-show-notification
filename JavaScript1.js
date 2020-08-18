155c227f4e30b4a2e2868bf98031ce3503a1aa6b
Abdullah Hussain <abdnhussain@gmail.com>
2020-08-18 15:36:43 +0100

    code to show notification

---
+++ JavaScript1.js
@@ -0,0 +1,15 @@
+﻿function shownotificationforsubscriptionrenewal1() {
+    var currentdate = new Date();
+    var enddate = Xrm.Page.getAttribute("ntt_enddate").getValue();
+
+    var diffintime = enddatetime - currentdate;
+    var diffindays = diffintime / (1000 * 3600 * 24);
+
+    if (enddate != null) {
+        if (diffindays <= 30) {
+            Xrm.Page.ui.setFormNotification("This subscription is due for renewal within " + diffindays + " days", "WARNING");
+        } else {
+            Xrm.Page.ui.clearFormNotification("WARNING");
+        }
+    }
+}
